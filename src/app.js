536e96451df220230cd4fe726260b36cde1ef4ba
mlodypies <simsd1234@gmail.com>
2023-09-18 13:52:22 +0200

    cc

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+
+const app = express();
+
+app.set('views', './src/views')
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    //res.send("strona dziala");
+    res.render('index', {
+        pageTitle: 'Strona glowna'
+    });
+});
+
+
+app.get('/omnie', (req, res) => {
+    res.render("omnie" , {
+        pageTitle: 'Strona o mnie'
+
+    });
+});
+
+
+app.get('/kontakt', (req, res) => {
+    res.render('kontakt', {
+        pageTitle: 'Strona kontakt'
+
+    });
+});
+
+app.listen(5000, () =>  {
+    console.log("serwer dziala");
+});
